b784d1840ae0bcac7158ff886b078d42fecf565a
Pr-DuyTam <prduytam@gmail.com>
2024-08-17 08:43:14 +0700

    a

---
+++ Bai2-SoThuc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	float a = 5.25;
+	float b = 4.25;
+	float c;
+	
+	cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+	c = a + b;
+	cout << "Phep cong " << a << " + " << b << " = " << c << endl;
+	
+	cout << "Phep tru " << a << " - " << b << " = " << c << endl;
+	
+	cout << "Phep nhan " << a << " * " << b << " = " << c << endl;
+	
+	cout << "Phep chia " << a << " / " << b << " = " << c << endl;
+	return 0;
+}
