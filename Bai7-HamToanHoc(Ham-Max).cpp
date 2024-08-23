69c3b82ff8f81a2a671d2e0ffe9ef61eddc7fd61
Pr-DuyTam <prduytam@gmail.com>
2024-08-23 20:14:11 +0700

    a

---
+++ Bai7-HamToanHoc(Ham-Max).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int a = 5, b = 10;
+	cout << "Max ("<<a<<","<<b<<") = " << max (a,b) <<endl;
+	
+	return 0;
+}
