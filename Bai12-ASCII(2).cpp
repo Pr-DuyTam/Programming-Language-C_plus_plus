4d959af15610a4e73b37e9abc921d5b796095560
Pr-DuyTam <prduytam@gmail.com>
2024-08-29 16:08:46 +0700

    a

---
+++ Bai12-ASCII(2).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char kt1 = 'A';
+	int n = kt1 + 10; // 65 + 10 = 75
+	cout << n <<endl;
+	
+	++kt1; // 66 -> B
+	cout << (int)kt1 << endl;
+	cout << kt1 << endl;
+	return 0; 
+}
+
---
+++ Bai12-ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char kt1 = 'A', kt2 = 'a', kt3 = '0';
+	cout << (int)kt1 << " "	<< (int)kt2 << " " << (int)kt3 << " " <<endl;
+	
+	int n = kt1;
+	cout << n << endl;
+	cout << kt1 + 100 <<endl;
+	
+	return 0;
+}
